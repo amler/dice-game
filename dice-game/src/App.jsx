5c43aff955ee1b2fb988dca04b14fa2a61e99d07
Heather Amler <amler.heather@gmail.com>
2023-11-28 10:56:07 -0500

    Adds another variation checking if every number of dice

--- dice-game/src/App.jsx
+++ dice-game/src/App.jsx
@@ -5,10 +5,15 @@ function lessThan4(dice) {
   return sum(dice) < 4;
 }
 
+function allSame(dice) {
+  return dice.every((v) => v === dice[0]);
+}
+
 function App() {
   return (
     <>
       <LuckyN winCheck={lessThan4}/>
+      <LuckyN winCheck={allSame}/>
     </>
   )
 }
